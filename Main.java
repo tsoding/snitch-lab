bde490efa9dbb9f485c608ae5eec877269dc9fec
rexim <reximkut@gmail.com>
2018-10-31 01:27:57 +0700

    Remove TODO(#3)

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO(#3): Rust is better than Go
         // TODO(#4): Actually Haskell is better
         System.out.println("Hello, World!");
     }
