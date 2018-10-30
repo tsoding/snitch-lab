ac6eb9078a91cbb9c9e708d5ac14f05a2de05c1d
rexim <reximkut@gmail.com>
2018-10-31 01:15:43 +0700

    Remove TODO(#1)

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO(#1): Rewrite this in Rust ffs
         // TODO(#3): Rust is better than Go
         System.out.println("Hello, World!");
     }
