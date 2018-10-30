51a8b8b4b3d294b21870ad9d949cb0765e1072b5
rexim <reximkut@gmail.com>
2018-10-31 01:27:26 +0700

    Add TODO(#4)

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#3): Rust is better than Go
+        // TODO(#4): Actually Haskell is better
         System.out.println("Hello, World!");
     }
 }
