4319bdfe26e5eaf99a0e83ffad68976fa70ba0e1
rexim <reximkut@gmail.com>
2018-11-07 01:13:30 +0700

    Add TODO(#5)

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#4): Actually Haskell is better
+        // TODO(#5): vim is better than Emacs
         System.out.println("Hello, World!");
     }
 }
