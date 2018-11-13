cf1f816176dbb12364a5ceb5365980d335aa7eb5
rexim <reximkut@gmail.com>
2018-11-13 23:11:38 +0700

    Remove TODO(#5)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#4): Actually Haskell is better
-        // TODO(#5): vim is better than Emacs
         System.out.println("Hello, World!");
     }
 }
