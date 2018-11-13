e3972bdbe4189350c9eafc75c1fd8f36ddedbf06
rexim <reximkut@gmail.com>
2018-11-14 00:17:08 +0700

    Remove TODO(#7)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#6): time management is not working for RainbowDeerWhatever
-        // TODO(#7): duckofdoom12 is stressed about jobs
         System.out.println("Hello, World!");
     }
 }
