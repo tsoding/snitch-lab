0e2c46ba0df10599f980270d43759cd8398e67cc
rexim <reximkut@gmail.com>
2018-11-14 00:15:01 +0700

    Add TODO(#8)

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#6): time management is not working for RainbowDeerWhatever
         // TODO(#7): duckofdoom12 is stressed about jobs
-        // TODO: tsoding drinks tea from a bag
+        // TODO(#8): tsoding drinks tea from a bag
         // TODO: VoightKampfPhil's code is mess
         System.out.println("Hello, World!");
     }
