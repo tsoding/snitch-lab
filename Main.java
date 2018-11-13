85fb10ceaab2874f2cecc832a52210f3ab9a243e
rexim <reximkut@gmail.com>
2018-11-14 00:17:08 +0700

    Remove TODO(#6)

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO(#6): time management is not working for RainbowDeerWhatever
         System.out.println("Hello, World!");
     }
 }
