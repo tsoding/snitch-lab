c07c3673418f009894596b7d162576f69db039b3
rexim <reximkut@gmail.com>
2018-11-17 13:57:21 +0700

    Add TODO(#11)

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#10): test 1
-        // TODO: test 2
+        // TODO(#11): test 2
         // TODO: test 3
         System.out.println("Hello, World!");
     }
