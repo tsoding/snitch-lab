54ca095465d433879d31252433ed7918ef2071be
rexim <reximkut@gmail.com>
2018-11-20 23:01:33 +0700

    Add TODO(#14)

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#13): implement this
-        // TODO: implement that
+        // TODO(#14): implement that
         // TODO: OMEGALUL
         System.out.println("Hello, World!");
     }
