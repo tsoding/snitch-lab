706eebf6c5a41c4e2349bf41483309f872c54338
rexim <reximkut@gmail.com>
2018-11-20 23:03:06 +0700

    Remove TODO(#14)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#13): implement this
-        // TODO(#14): implement that
         System.out.println("Hello, World!");
     }
 }
