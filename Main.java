8d75d85f60d48887fc44cc001cf7e513a63bbbcf
rexim <reximkut@gmail.com>
2018-11-28 01:12:22 +0700

    Remove TODO(#24)

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ public class Main {
         // TODO(#21): puka doesn't have a life
         // TODO(#22): Not being trusted in tsoding's chat
         // TODO(#23): X33Grawl doesn't have idea for the next project
-        // TODO(#24): mateossh uses Windows
         System.out.println("Hello, World!");
     }
 }
