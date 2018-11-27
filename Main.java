ea60f0089269bd1dd3f256f4ac29231a13bbb51f
rexim <reximkut@gmail.com>
2018-11-28 01:12:22 +0700

    Remove TODO(#22)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#21): puka doesn't have a life
-        // TODO(#22): Not being trusted in tsoding's chat
         System.out.println("Hello, World!");
     }
 }
