df35ae0fa2f5bda109a4b36acfb6b99fb8072f09
rexim <reximkut@gmail.com>
2018-11-28 01:12:22 +0700

    Remove TODO(#23)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#21): puka doesn't have a life
         // TODO(#22): Not being trusted in tsoding's chat
-        // TODO(#23): X33Grawl doesn't have idea for the next project
         System.out.println("Hello, World!");
     }
 }
