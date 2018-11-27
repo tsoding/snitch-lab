66266b160b75530edbb88e672a588fc3bf414bc2
rexim <reximkut@gmail.com>
2018-11-28 01:01:29 +0700

    Add TODO(#23)

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#21): puka doesn't have a life
         // TODO(#22): Not being trusted in tsoding's chat
-        // TODO: X33Grawl doesn't have idea for the next project
+        // TODO(#23): X33Grawl doesn't have idea for the next project
         // TODO: mateossh uses Windows
         // TODO: apa got the flu
         System.out.println("Hello, World!");
