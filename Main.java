77212d563e067743fcb8c2344491c76da9dfcb7e
rexim <reximkut@gmail.com>
2018-11-27 22:58:39 +0700

    Add TODO(#16)

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        // TODO(#16): kankan988's social life
+        // TODO: empty bank
+        // TODO: elm
+        // TODO: this is not enterprise enough
+        // TODO: Erectile dysfunction
         System.out.println("Hello, World!");
     }
 }
