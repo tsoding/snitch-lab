b18d305a6ce8af0da372915d8a75edc8129b4616
rexim <reximkut@gmail.com>
2018-11-27 23:00:32 +0700

    Remove TODO(#18)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#16): kankan988's social life
         // TODO(#17): empty bank
-        // TODO(#18): elm
         System.out.println("Hello, World!");
     }
 }
