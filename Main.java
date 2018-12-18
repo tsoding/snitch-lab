ec2f6fd8a9dc14bb8b22245716f7631ecfce8036
rexim <reximkut@gmail.com>
2018-12-19 00:12:47 +0700

    Add TODO(#27)

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#26): hello world
+        // TODO(#27): 1um6okote's diploma
         System.out.println("Hello, World!");
     }
 }
