09d91a6e5522c12f69a38ec291b27d1c2f333bbb
rexim <reximkut@gmail.com>
2018-12-19 00:31:44 +0700

    Add TODO(#28)

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#26): hello world
         // TODO(#27): 1um6okote's diploma
+        // TODO(#28): rewrite in rust
+        // TODO: Fix whole PHP
+        // TODO: Fix 1um6okote's brain
         System.out.println("Hello, World!");
     }
 }
