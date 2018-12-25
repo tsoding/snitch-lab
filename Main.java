f7d0d272f61b73fbff6a21f6aa1942f375b4dd69
rexim <reximkut@gmail.com>
2018-12-26 01:06:18 +0700

    Remove TODO(#28)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#26): hello world
         // TODO(#27): 1um6okote's diploma
-        // TODO(#28): rewrite in rust
         System.out.println("Hello, World!");
     }
 }
