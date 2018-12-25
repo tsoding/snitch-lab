c12def011ec1b9ca88cd6c9c209430c88098968c
rexim <reximkut@gmail.com>
2018-12-26 01:06:18 +0700

    Remove TODO(#27)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#26): hello world
-        // TODO(#27): 1um6okote's diploma
         System.out.println("Hello, World!");
     }
 }
