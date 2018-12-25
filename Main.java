fc209ef48233d8a6b8aff8e15ebd3a0978a5e524
rexim <reximkut@gmail.com>
2018-12-26 01:06:18 +0700

    Remove TODO(#29)

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ public class Main {
         // TODO(#26): hello world
         // TODO(#27): 1um6okote's diploma
         // TODO(#28): rewrite in rust
-        // TODO(#29): Fix whole PHP
         System.out.println("Hello, World!");
     }
 }
