7c2f78728a0b583c2002b5da895009e2b936a483
rexim <reximkut@gmail.com>
2018-12-26 01:08:27 +0700

    Add TODO(#35)

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#34): Grawl does not know how to use TIG
-        // TODO: Funkschy is on Twitch during Christmas
+        // TODO(#35): Funkschy is on Twitch during Christmas
         System.out.println("Hello, World!");
     }
 }
