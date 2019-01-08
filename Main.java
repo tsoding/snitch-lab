f5c50d66a3a83ef4acefcdaeb3fab0a86f1068f9
rexim <reximkut@gmail.com>
2019-01-09 00:25:27 +0700

    Remove TODO(#35)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#34): Grawl does not know how to use TIG
-        // TODO(#35): Funkschy is on Twitch during Christmas
         System.out.println("Hello, World!");
     }
 }
