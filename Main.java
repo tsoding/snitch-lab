adf20c825725ff7c708c63a9872307b13caab8c0
rexim <reximkut@gmail.com>
2019-01-09 00:38:47 +0700

    Add FIXME(#36)

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // FIXME(#36): microbicmusic needs a C++ job
+        // XXX: Kubissus has an erectile disfunction
+        // @todo: NTSC audio capture in abridgewater's OBS setup desyncs about 3/4 of a second per hour
         System.out.println("Hello, World!");
     }
 }
