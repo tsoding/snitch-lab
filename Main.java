85c490a98093fc3c5059d35317cd088a408f4ffb
rexim <reximkut@gmail.com>
2019-01-09 02:15:15 +0700

    Remove @todo(#37)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // XXX(#38): Kubissus has an erectile disfunction
-        // @todo(#37): NTSC audio capture in abridgewater's OBS setup desyncs about 3/4 of a second per hour
         System.out.println("Hello, World!");
     }
 }
