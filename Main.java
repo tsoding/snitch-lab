a592054c3bb1694efd4ae0c9530a9a031495fa5d
rexim <reximkut@gmail.com>
2019-01-09 02:16:19 +0700

    Add TODO(#39)

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // TODO(#39): Hello World
+        // Foo
+        // Bar
+        // Baz
+        // TODO: Foo Bar
+        // TODO: Test Test
         System.out.println("Hello, World!");
     }
 }
