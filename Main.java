c1cfdd6b97f073006012881f4db5b0b9a0e45e8e
rexim <reximkut@gmail.com>
2019-01-09 00:39:05 +0700

    Add XXX(#38)

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // FIXME(#36): microbicmusic needs a C++ job
-        // XXX: Kubissus has an erectile disfunction
+        // XXX(#38): Kubissus has an erectile disfunction
         // @todo(#37): NTSC audio capture in abridgewater's OBS setup desyncs about 3/4 of a second per hour
         System.out.println("Hello, World!");
     }
