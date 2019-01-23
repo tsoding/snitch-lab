2e1423c2d8ac13f2665f9a19a69b6f17c336aaf1
rexim <reximkut@gmail.com>
2019-01-23 10:13:56 +0700

    Remove TODO(#47)

--- Main.java
+++ Main.java
@@ -1,10 +1,4 @@
 public class Main {
-    // TODO(#47): Capitalized Word
-    // If a capitalized word is a word whose first letter is in
-    // uppercase/majuscule and any following letters are in
-    // lowercase/miniscules, then the opposite of that must be a word
-    // whose first letter is in lowercase/miniscule and any following
-    // letters are in uppercase/majuscules.
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
