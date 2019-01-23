858a0ec4ee44e778f35f78389de0bd15064af042
rexim <reximkut@gmail.com>
2019-01-23 10:15:42 +0700

    Add TODO(#50)

--- Main.java
+++ Main.java
@@ -3,6 +3,14 @@ public class Main {
         // TODO(#49): Foo Bar
         //   Foo
         //   Bar
+        // TODO(#50): Hello World
+        //   ```c
+        //   int main()
+        //   {
+        //       printf("Hello, World");
+        //       return 0;
+        //   }
+        //   ```
         System.out.println("Hello, World!");
     }
 }
