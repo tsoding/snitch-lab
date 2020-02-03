d754df8157a711c429888a95966b52ef93d959be
rexim <reximkut@gmail.com>
2020-02-03 09:11:20 +0700

    Remove TODO(#63)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#61): 1
         // TODO(#62): 2
-        // TODO(#63): 3
         System.out.println("Hello, World!");
     }
 }
