47020b47a615cf259c3a529f75565ba1862f7d65
rexim <reximkut@gmail.com>
2020-02-03 09:10:49 +0700

    Add TODO(#61)

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // TODO(#61): 1
+        // TODO: 2
+        // TODO: 3
         System.out.println("Hello, World!");
     }
 }
