de58c385818316ac8c781f451b6d61a3d740868d
rexim <reximkut@gmail.com>
2020-07-13 02:59:26 +0700

    Add TODO(#66)

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#64): apa420's mysql user does not work
         // TODO(#65): rewrite this in Rust
-        // TODO: fix typo in readme
+        // TODO(#66): fix typo in readme
         System.out.println("Hello, World!");
     }
 }
