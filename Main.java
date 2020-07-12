56ff900d0b596e5430f72cb4a6caf19c909daaa5
rexim <reximkut@gmail.com>
2020-07-13 03:00:11 +0700

    Remove TODO(#65)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#64): apa420's mysql user does not work
-        // TODO(#65): rewrite this in Rust
         System.out.println("Hello, World!");
     }
 }
