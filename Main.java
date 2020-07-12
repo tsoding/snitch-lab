5acd49eb20030151be44ec39d6298e23a4f17a67
rexim <reximkut@gmail.com>
2020-07-13 03:01:46 +0700

    Add TODO(#67)

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        // TODO(#67): fix my depression
+        // TODO: im fat
+        // TODO: not enough factories
+        // TODO: twitch is slow
         System.out.println("Hello, World!");
     }
 }
