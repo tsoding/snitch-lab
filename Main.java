f51c1b351f5becb525af9826547327b6298c9c39
rexim <reximkut@gmail.com>
2020-07-13 03:11:16 +0700

    Remove TODO(#67)

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO(#67): fix my depression
         // TODO: twitch is slow
         System.out.println("Hello, World!");
     }
