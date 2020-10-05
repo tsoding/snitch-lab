cc0a5cf07ccedff5e9743696aa2451ed36fbaa42
rexim <reximkut@gmail.com>
2020-10-05 18:48:39 +0700

    Remove TODO(#87)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#86): quit smoking
-        // TODO(#87): Magic
         System.out.println("Hello, World!");
     }
 }
