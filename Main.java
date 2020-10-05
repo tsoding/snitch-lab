fa0047bbb1e6f0acef921dfa51113690b454d063
rexim <reximkut@gmail.com>
2020-10-05 18:48:39 +0700

    Remove TODO(#88)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#86): quit smoking
         // TODO(#87): Magic
-        // TODO(#88): install gentoo
         System.out.println("Hello, World!");
     }
 }
