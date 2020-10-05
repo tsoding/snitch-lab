24fe536bc8cc8ca1f0c3b0bff95d758b21c58c50
rexim <reximkut@gmail.com>
2020-10-05 18:33:58 +0700

    Remove TODO(#84)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#82): submit todos
         // TODO(#83): you should ask money for that Kapp
-        // TODO(#84): error handling?
         System.out.println("Hello, World!");
     }
 }
