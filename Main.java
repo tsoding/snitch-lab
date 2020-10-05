9ef7b115b6afd81d26a7df1717bc80cc0d7fa4b3
rexim <reximkut@gmail.com>
2020-10-05 17:47:33 +0700

    Remove TODO(#80)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#78): ur mom
         // TODO(#79): greet universe
-        // TODO(#80): prepare for tea break
         System.out.println("Hello, World!");
     }
 }
