7f741d87392e678d3bbdb0f295657ed11fc70e3d
rexim <reximkut@gmail.com>
2020-10-05 17:23:26 +0700

    Add TODO(#76)

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#74): twitch is slow
         // TODO(#75): Rewrite this in Rust
-        // TODO: make tea
+        // TODO(#76): make tea
         // TODO: No testing api github
         System.out.println("Hello, World!");
     }
