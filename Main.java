bd727b305fc01d05a7fb7c8e8ff9a9c776cdfe0f
rexim <reximkut@gmail.com>
2020-10-05 18:48:11 +0700

    Add TODO(#88)

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#86): quit smoking
         // TODO(#87): Magic
-        // TODO: install gentoo
+        // TODO(#88): install gentoo
         // TODO: prove Rieman Hypothesis
         // TODO: start drinking
         System.out.println("Hello, World!");
