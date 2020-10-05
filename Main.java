a03b3c834891bc9c5cc6b5c7cba6f06a3e513e11
rexim <reximkut@gmail.com>
2020-10-05 18:29:28 +0700

    Add TODO(#83)

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO(#82): submit todos
-        // TODO: you should ask money for that Kapp
+        // TODO(#83): you should ask money for that Kapp
         // TODO: error handling?
         // TODO: rewrite in haskell
         System.out.println("Hello, World!");
