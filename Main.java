de8bfee316bf0e6426b2f6fbfa832bd5de95a278
rexim <reximkut@gmail.com>
2020-10-05 18:48:39 +0700

    Remove TODO(#89)

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ public class Main {
         // TODO(#86): quit smoking
         // TODO(#87): Magic
         // TODO(#88): install gentoo
-        // TODO(#89): prove Rieman Hypothesis
         System.out.println("Hello, World!");
     }
 }
