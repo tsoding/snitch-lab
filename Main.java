31ea081fe30e89c603a0a772a3b950773d672e96
rexim <reximkut@gmail.com>
2020-10-05 17:42:51 +0700

    Add TODO(#80)

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#78): ur mom
         // TODO(#79): greet universe
-        // TODO: prepare for tea break
+        // TODO(#80): prepare for tea break
         // TODO: p = np
         System.out.println("Hello, World!");
     }
