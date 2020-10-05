8cfe7d44d5c593399c9842ca9cb9ec09c1e8dad0
rexim <reximkut@gmail.com>
2020-10-05 17:24:42 +0700

    Remove TODO(#76)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#74): twitch is slow
         // TODO(#75): Rewrite this in Rust
-        // TODO(#76): make tea
         System.out.println("Hello, World!");
     }
 }
