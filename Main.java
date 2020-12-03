a2c077218e021ae8c6a74d41cb2f24eab1a6703d
rexim <reximkut@gmail.com>
2020-12-03 18:56:40 +0700

    Add TODO(#4)

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO(#2): foo
         // TODO(#3): bar
-        // TODO: baz
+        // TODO(#4): baz
         System.out.println("Hello, World!");
     }
 }
