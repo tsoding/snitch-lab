5852ebc2a04835cd0d96e33761cc19bb1b70c167
rexim <reximkut@gmail.com>
2021-11-23 17:13:17 +0700

    Remove TODO(#103)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: bar
-        // TODO(#103): hello,
         // TODO(#104): world
         System.out.println("Hello, World!");
     }
