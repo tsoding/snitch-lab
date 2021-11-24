119ff55e1b6d19d99fc9836ebd566e1edc2d22a4
rexim <reximkut@gmail.com>
2021-11-24 17:45:53 +0700

    Remove TODO(#112)

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 public class Main {
     // TODOOOO(#111): first
     public static void main(String[] args) {
-        // TODOOO(#112): second
         System.out.println("Hello, World!");
     }
 }
